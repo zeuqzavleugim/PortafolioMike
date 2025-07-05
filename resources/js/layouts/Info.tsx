20359979460928f484bf170c8b5205b112d238f3
Miguel <143663845+zeuqzavleugim@users.noreply.github.com>
2025-07-04 23:53:55 -0600

    Add responsive profile section and update Info layout
    Introduced new CSS classes for a responsive profile section with media queries for different screen sizes. Updated Info.tsx to use these classes, display a profile image, and add contact/social icons with links. Improved layout and accessibility for better user experience.

--- resources/js/layouts/Info.tsx
+++ resources/js/layouts/Info.tsx
@@ -1,38 +1,56 @@
+import { General } from "@/imgs/general";
+import { Link } from "@inertiajs/react";
+import { Icons } from "@/imgs/Icons";
+
 export default function Info() {
     return(
         <>
-            <header>
-                <div>
+            <header className="first-seccion">
+                <div className="seccion-img-perfil">
                     <picture>
-                        <img src="" alt="" title="" loading="lazy" />
+                        <img 
+                            src={General.Perfil} 
+                            alt="Foto de Perfil estilo Funco" 
+                            title="Foto de Perfil estilo Funco" 
+                            loading="lazy" />
                     </picture>
                 </div>
-                <div>
+                <div className="seccion-info-perfil">
                     <h1>
                         Diseño y Desarrollo Web con SEO  Impulsa tu 
                         Negocio Online
                     </h1>
                     <p>
                         Desarrollo sitios y aplicaciones web modernas, 
-                        rápidas y listas para posicionarse en Google. 
-                        <br />
+                        rápidas y listas para posicionarse en Google.
+                        <br /><br /> 
                         Trabajo con tecnologías como Laravel y React, 
-                        cuidando cada detalle desde el código hasta la 
-                        experiencia del usuario, para que tu negocio 
-                        no solo luzca bien, sino venda. 
-                        <br />
+                        cuidando cada detalle desde el código hasta 
+                        la experiencia del usuario, para que tu 
+                        negocio no solo luzca bien, sino venda. 
+                        <br /><br />
                         Aplico estrategias SEO, analizo el rendimiento 
                         con herramientas de Google y transformo tus 
                         ideas en soluciones digitales que atraen 
                         clientes. 
-                        <br />
-                        Ya sea que necesites un portafolio simple o 
-                        un sitio completo para impulsar tu empresa, 
-                        estoy aquí para ayudarte a crecer online y 
-                        preparar el camino hacia un marketing digital 
-                        a otro nivel.
+                        <br /><br />
+                        Ya sea que necesites un portafolio 
+                        simple o un sitio completo para impulsar tu 
+                        empresa, estoy aquí para ayudarte a crecer 
+                        online y preparar el camino hacia un 
+                        marketing digital a otro nivel.
                     </p>
-                    <div></div>
+                    <div className="seccion-icons">
+                        <a href="">
+                            <img src={Icons.Correo} alt="Contacto" title="Contacto" />
+                        </a>
+                        <a href="https://github.com/zeuqzavleugim" target="_blank">
+                            <img src={Icons.Github} alt="GitHub" title="GitHub"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/miguel-angel-vazquez-aviles-170abb15a/" target="_blank">
+                            <img src={Icons.Linkedin} alt="Linkedin" title="Linkedin"/>
+                        </a>
+                    </div>
                 </div>
             </header>
         </>
