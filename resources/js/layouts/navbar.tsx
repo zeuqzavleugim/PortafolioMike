c85851c0ca575f0107af97d63a17a5ea627ef87f
Miguel <143663845+zeuqzavleugim@users.noreply.github.com>
2025-07-09 23:01:40 -0600

    Refactor navbar to use Bootstrap offcanvas menu
    Updated the Navbar component to implement a Bootstrap 5 offcanvas menu for improved mobile navigation. Also removed duplicate logo and toggler code. Minor duplicate Bootstrap CSS and JS includes were added in app.blade.php, possibly unintentionally.

--- resources/js/layouts/navbar.tsx
+++ resources/js/layouts/navbar.tsx
@@ -4,19 +4,55 @@ import { Icons } from "@/imgs/Icons";
 export default function Navbar() {
     return(
         <>
+        <nav className="navbar bg-body-tertiary fixed-top">
+            <div className="container-fluid">
+                <Link className="navbar-brand" href="#inicio" title="maykcoffe">
+                    <img 
+                        src={Icons.Logo} 
+                        className="iconsLogo" 
+                        alt="Logotipo" 
+                        title="Logotipo" 
+                        loading="lazy"/>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="offcanvas-header">
+                    <h4 className="offcanvas-title" id="offcanvasNavbarLabel">
+                        <img 
+                        src={Icons.Logo} 
+                        className="iconsLogo" 
+                        alt="Logotipo" 
+                        title="Logotipo" 
+                        loading="lazy"/>
+                    </h4>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li className="nav-item">
+                        <Link className="nav-link" href="#desarrollos" title="desarrollos">Desarrollos</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" href="#conoceme" title="conoceme">Conoceme</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" href="#" title="servicios">Servicios</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" href="#planes" title="planes">Planes</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" href="#contacto" title="contacto">Contacto</Link>
+                    </li>
+                    </ul>
+                </div>
+                </div>
+            </div>
+        </nav>
             <nav className="navbar navbar-expand-lg" id="inicio">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" href="#inicio" title="maykcoffe">
-                        <img 
-                            src={Icons.Logo} 
-                            className="iconsLogo" 
-                            alt="Logotipo" 
-                            title="Logotipo" 
-                            loading="lazy"/>
-                    </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav">
                             <li className="nav-item">
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,7 +28,8 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
         @routes
         @viteReactRefresh
