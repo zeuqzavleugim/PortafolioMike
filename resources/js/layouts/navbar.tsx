35e165c47d674847fc07c626b13d1cea09815b99
Miguel <143663845+zeuqzavleugim@users.noreply.github.com>
2025-07-12 11:53:48 -0600

    Refactor navbar layout and update responsive styles
    Reorganized the navbar structure in navbar.tsx to consolidate navigation and toggler logic, improving maintainability. Updated app.css to enhance responsive behavior for different screen sizes, adjusting navbar and logo display, and refining breakpoints for better mobile and tablet support.

--- resources/js/layouts/navbar.tsx
+++ resources/js/layouts/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return(
         <>
         <nav className="navbar navbar-contraint">
-            <div className="container-fluid">
+            <div className="container-fluid nav-var-expain">
                 <Link className="navbar-brand" href="#inicio" title="maykcoffe">
                     <img 
                         src={Icons.Logo} 
@@ -14,44 +14,6 @@ export default function Navbar() {
                         title="Logotipo" 
                         loading="lazy"/>
                 </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                <div className="offcanvas-header">
-                    <h4 className="offcanvas-title" id="offcanvasNavbarLabel">
-                        <img 
-                        src={Icons.Logo} 
-                        className="iconsLogo" 
-                        alt="Logotipo" 
-                        title="Logotipo" 
-                        loading="lazy"/>
-                    </h4>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div className="offcanvas-body">
-                    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                    <li className="nav-item">
-                        <Link className="nav-link" href="#desarrollos" title="desarrollos">Desarrollos</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" href="#conoceme" title="conoceme">Conoceme</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" href="#" title="servicios">Servicios</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" href="#planes" title="planes">Planes</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" href="#contacto" title="contacto">Contacto</Link>
-                    </li>
-                    </ul>
-                </div>
-                </div>
-            </div>
-        </nav>
-            <nav className="nav-var-expain" id="inicio">
                 <ul className="navbar-nava">
                     <li className="nav-item">
                         <Link className="nav-link" href="#desarrollos" title="desarrollos">Desarrollos</Link>
@@ -69,7 +31,43 @@ export default function Navbar() {
                         <Link className="nav-link" href="#contacto" title="contacto">Contacto</Link>
                     </li>
                 </ul>
-            </nav>
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div className="offcanvas-header">
+                        <h4 className="offcanvas-title" id="offcanvasNavbarLabel">
+                            <img 
+                            src={Icons.Logo} 
+                            className="iconsLogo" 
+                            alt="Logotipo" 
+                            title="Logotipo" 
+                            loading="lazy"/>
+                        </h4>
+                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div className="offcanvas-body">
+                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#desarrollos" title="desarrollos">Desarrollos</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#conoceme" title="conoceme">Conoceme</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#" title="servicios">Servicios</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#planes" title="planes">Planes</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" href="#contacto" title="contacto">Contacto</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
         </>
     );
 }
